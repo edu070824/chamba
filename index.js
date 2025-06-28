ae8c0ba6c9c19bda0ff697ac3413d35c52d4e510
edu070824 <llanosedu16@gmail.com>
2025-06-28 06:47:09 -0500

    borre y reinicie desde cero

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    fs.createReadStream('./static/index.html')
+    read.pipe(res)
+})
+
+server.listen(3000)
+console.log(`Server on port ${3000}`)
