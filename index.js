39990928b59c860dd25fccb91002335dbdf23752
edu070824 <llanosedu16@gmail.com>
2025-06-29 13:59:26 -0500

    change in master

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-var nombre = "Jose Gomez LLANOS SANCHEZ BRISA Florez";
+var nombre = "ESTAMOS EN MASTER CON LOS CAMBIOS EN LOGIN LLANOS SANCHEZ BRISA Florez";
 console.log("mi nombre es " + nombre);
